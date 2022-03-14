f8ebc9426e518541a82ee63594b29ce27cc8d119
Alberto Finardi <finardialberto@gmail.com>
2022-03-14 13:54:16 +0100

    03_14

--- 03_14/Es 2.4/udp_server.py
+++ 03_14/Es 2.4/udp_server.py
@@ -1,5 +1,18 @@
 from socket import socket, AF_INET, SOCK_DGRAM
 
+def primo(k):
+    if k > 1:
+        for i in range(2, int(k / 2) + 1):
+            if (k % i) == 0:
+                response = 'Numero non primo'
+                break
+        else:
+            response = 'Numero primo'
+    else:
+        response = 'Numero non primo'
+
+    return response
+
 def main():
     serverPort = 12500
 
@@ -16,22 +29,9 @@ def main():
         # 2048 = dimensione buffer di ricezione -> deve essere >= del contenuto trasmesso (sennò informazione persa)
         print(f'Connessione: {clientAddress[0]}:{clientAddress[1]}')
         message = message.decode('utf-8')
-        k = int(message)
-        response = ''
-
-        if k > 1:
-            for i in range(2, int(k / 2) + 1):
-                if (k % i) == 0:
-                    response = 'Numero non primo'
-                    break
-            else:
-                response = 'Numero primo'
-        else:
-            response = 'Numero non primo'
+        response = primo(int(message))
 
         serverSocket.sendto(response.encode('utf-8'), clientAddress)
-
-
 
 if __name__ == '__main__':
     main()
